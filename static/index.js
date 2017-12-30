64e70232506fb0f009d1501c34fbd2648a19319f
Kirill Novik <jimmynovik@gmail.com>
2017-12-30 16:10:00 +0000

    Add Hierarchy Display Logic

--- static/index.js
+++ static/index.js
@@ -1,5 +1,73 @@
-function greeter(person) {
-    return "Hello, " + person;
-}
-var user = [0, 1, 2];
-$.find("#div001").html(greeter(user));
+var HierarchyController = (function () {
+    function HierarchyController(hierarchyModel, hierarchyView) {
+        this.hierarchyModel = hierarchyModel;
+        this.hierarchyView = hierarchyView;
+        this.display();
+    }
+    HierarchyController.prototype.display = function () {
+        this.hierarchyView.display(this.hierarchyModel);
+    };
+    return HierarchyController;
+}());
+var HierarchyModel = (function () {
+    function HierarchyModel(hierarchyTree) {
+        this.hierarchyTree = hierarchyTree;
+    }
+    HierarchyModel.prototype.getHierarchyTreeAsObject = function () {
+        return this.hierarchyTree;
+    };
+    HierarchyModel.prototype.getHierarchyTreeAsString = function () {
+        var result = "";
+        return JSON.stringify(this.hierarchyTree);
+    };
+    HierarchyModel.prototype.getHierarchyTree = function () {
+        var result = "";
+        this.hierarchyTree.iterate(this.hierarchyTree.hierarchyRoot, function (node) {
+            result += " " + node.name + " ";
+        });
+        return result;
+    };
+    HierarchyModel.prototype.setHierarchyTree = function (hierarchyTree) {
+        this.hierarchyTree = hierarchyTree;
+    };
+    return HierarchyModel;
+}());
+var HierarchyView = (function () {
+    function HierarchyView(hierarchyWindow) {
+        this.hierarchyWindow = hierarchyWindow;
+    }
+    HierarchyView.prototype.display = function (hierarchyModel) {
+        this.hierarchyWindow.html(hierarchyModel.getHierarchyTree());
+    };
+    return HierarchyView;
+}());
+var HierarchyTree = (function () {
+    function HierarchyTree(hierarchyRoot) {
+        this.hierarchyRoot = hierarchyRoot;
+    }
+    HierarchyTree.prototype.iterate = function (currentNode, callback) {
+        if (currentNode) {
+            callback(currentNode);
+            for (var _i = 0, _a = currentNode.children; _i < _a.length; _i++) {
+                var childNode = _a[_i];
+                this.iterate(childNode, callback);
+            }
+        }
+        else {
+            return;
+        }
+    };
+    return HierarchyTree;
+}());
+var HierarchyNode = (function () {
+    function HierarchyNode(name, children) {
+        this.name = name;
+        this.children = children;
+    }
+    return HierarchyNode;
+}());
+var node001 = new HierarchyNode("node001", []);
+var hierarchyTree = new HierarchyTree(node001);
+var hierarchyModel = new HierarchyModel(hierarchyTree);
+var hierarchyView = new HierarchyView($("#div001"));
+var hierarchyController = new HierarchyController(hierarchyModel, hierarchyView);
--- static/index.ts
+++ static/index.ts
@@ -1,7 +1,83 @@
-function greeter(person: string) {
-    return "Hello, " + person;
+class HierarchyController {
+    hierarchyModel: HierarchyModel
+    hierarchyView: HierarchyView
+    constructor(hierarchyModel: HierarchyModel, hierarchyView: HierarchyView) {
+        this.hierarchyModel = hierarchyModel;
+        this.hierarchyView = hierarchyView;
+        this.display();
+    }
+    display(){
+        this.hierarchyView.display(this.hierarchyModel);
+    }
 }
 
-let user = [0, 1, 2];
+class HierarchyModel {
+    hierarchyTree: HierarchyTree
+    constructor(hierarchyTree: HierarchyTree){
+        this.hierarchyTree = hierarchyTree;
+    }
+    getHierarchyTreeAsObject(){
+        return this.hierarchyTree;
+    }
+    getHierarchyTreeAsString(){
+        let result = "";
+        return JSON.stringify(this.hierarchyTree);
+    }
+    getHierarchyTree(){
+        let result = "";
+        this.hierarchyTree.iterate(
+            this.hierarchyTree.hierarchyRoot, 
+            (node: HierarchyNode) => {
+                result += " " + node.name + " ";
+            }
+        );
+        return result;
+    }
+    setHierarchyTree(hierarchyTree: HierarchyTree){
+        this.hierarchyTree = hierarchyTree;
+    }
+}
 
-$.find("#div001").html(greeter(user));
+class HierarchyView {
+    hierarchyWindow: Object
+    constructor(hierarchyWindow: Object){
+        this.hierarchyWindow = hierarchyWindow;
+    }
+    display(hierarchyModel: HierarchyModel){
+        this.hierarchyWindow.html(hierarchyModel.getHierarchyTree());
+    }
+}
+
+class HierarchyTree {
+    hierarchyRoot: HierarchyNode
+    constructor(hierarchyRoot: HierarchyNode){
+        this.hierarchyRoot = hierarchyRoot;
+    }
+    iterate(currentNode: HierarchyNode, callback: Function){
+        if (currentNode){
+            callback(currentNode);
+            for (let childNode of currentNode.children){
+                this.iterate(childNode, callback);
+            }    
+        } else {
+            return
+            
+        }
+    }
+}
+
+class HierarchyNode {
+    name: string
+    children: Array<HierarchyNode>
+    constructor(name: string, children: Array<HierarchyNode>){
+        this.name = name;
+        this.children = children;
+    }
+}
+
+let node001 = new HierarchyNode("node001", []);
+let hierarchyTree = new HierarchyTree(node001)
+
+let hierarchyModel = new HierarchyModel(hierarchyTree);
+let hierarchyView = new HierarchyView($("#div001"));
+let hierarchyController = new HierarchyController(hierarchyModel, hierarchyView);
